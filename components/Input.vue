6b32605c5f06c0844f24439701b01d86a5830d1d
Héuller <hllrdev@gmail.com>
2023-07-20 10:39:48 -0300

    macros page initialized

---
+++ components/Input.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="form-control pb-2">
+        <label class="label">
+            <span class="label-text text-neutral-600">{{ props.label }}</span>
+        </label>
+        <input :name="props.name" :type="props.type" :placeholder="props.placeholder" 
+            class="input w-full md:max-w-xs placeholder:text-sm border border-neutral-300 text-neutral-500 focus:outline-none focus:border-secondary" />
+    </div>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        name: {
+            type:String,
+            required:true
+        },
+        type: {
+            type: String,
+            default: "text"
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        placeholder: {
+            type: String,
+            required: true
+        }
+    })
+
+</script>
---
+++ pages/macros.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="pt-24 px-4 ">
+        <h1 class="text-center pb-6">Cálculo macronutrientes</h1>
+        <div class="md:flex justify-center">
+            <form action="" class="md:w-4/5 lg:w-3/5">
+                <Input name="age" type="number" label="Qual a sua idade(anos)?" placeholder="Digite aqui" />
+                <Input name="height" type="number" label="Qual a sua altura(cm)?" placeholder="Digite aqui" />
+                <Input name="weight" type="number" label="Qual o seu peso(kg)?" placeholder="Digite aqui" />
+            </form>
+        </div>
+    </div>
+</template>
