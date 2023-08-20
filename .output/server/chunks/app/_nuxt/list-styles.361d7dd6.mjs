3345cd65496faf3ce89980df6818a7b00d9c0f73
hllrdev <133055330+hllrdev@users.noreply.github.com>
2023-08-20 10:31:06 -0300

    Delete .output directory

--- .output/server/chunks/app/_nuxt/list-styles.361d7dd6.mjs
+++
@@ -1,6 +0,0 @@
-const list_vue_vue_type_style_index_0_scoped_b482803c_lang = "td[data-v-b482803c]{padding:.5rem}td *[data-v-b482803c]{margin:.25rem}tr th[data-v-b482803c]{color:#000}";
-
-const listStyles_361d7dd6 = [list_vue_vue_type_style_index_0_scoped_b482803c_lang, list_vue_vue_type_style_index_0_scoped_b482803c_lang];
-
-export { listStyles_361d7dd6 as default };
-//# sourceMappingURL=list-styles.361d7dd6.mjs.map
--- .output/server/chunks/app/_nuxt/main-styles.a8ec3973.mjs
+++
@@ -1,6 +0,0 @@
-const MenuItems_vue_vue_type_style_index_0_scoped_0791b2be_lang = "li[data-v-0791b2be]:hover{--tw-text-opacity:1;color:hsl(var(--p)/var(--tw-text-opacity))}";
-
-const mainStyles_a8ec3973 = [MenuItems_vue_vue_type_style_index_0_scoped_0791b2be_lang];
-
-export { mainStyles_a8ec3973 as default };
-//# sourceMappingURL=main-styles.a8ec3973.mjs.map
