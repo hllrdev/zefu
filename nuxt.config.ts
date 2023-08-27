8f246e4db4b7c98e6e9b944703053748be074415
Héuller <hllrdev@gmail.com>
2023-08-27 10:01:09 -0300

    att apiurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,7 @@ export default defineNuxtConfig({
     enabled: false
   },
   runtimeConfig: {
-    public: {
-      API_URL: process.env.API_URL
-    }
+    public: {API_URL: process.env.API_URL}
   },
   css: ['~/assets/css/main.css'],
   postcss: {
